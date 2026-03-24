91b01086fba170b8de70425a6844ba398c1facbc
Marten2007 <147500662+Marten2007@users.noreply.github.com>
2026-03-24 10:46:08 +0200

    yl uuendused

---
+++ yl1.ps1
@@ -0,0 +1,34 @@
+﻿# Küsi eesnimi ja perenimi
+$eesnimi = Read-Host "Sisesta oma eesnimi"
+$perenimi = Read-Host "Sisesta oma perenimi"
+
+$eesnimi = $eesnimi.ToLower()
+$perenimi = $perenimi.ToLower()
+
+$kasutajaNimi = "$eesnimi.$perenimi"
+
+Write-Host "Loodav kasutaja on $kasutajaNimi"
+
+$parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+
+$olemas = Get-LocalUser -Name $kasutajaNimi -ErrorAction SilentlyContinue
+
+if ($olemas) {
+    Write-Host "Kasutaja $kasutajaNimi on juba olemas." -ForegroundColor Yellow
+}
+else {
+    try {
+        New-LocalUser `
+        -Name $kasutajaNimi `
+        -Password $parool `
+        -FullName "$eesnimi $perenimi" `
+        -Description "Lokaalne kasutaja $eesnimi $perenimi" `
+        -ErrorAction Stop
+
+        Write-Host "Tekkinud probleem kasutaja loomisega ei ole." -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Kasutaja loomine ebaõnnestus." -ForegroundColor Red
+        Write-Host $_.Exception.Message
+    }
+}
---
+++ yl2.ps1
@@ -0,0 +1,32 @@
+﻿# Küsi eesnimi ja perenimi
+$eesnimi = Read-Host "Sisesta kasutaja eesnimi"
+$perenimi = Read-Host "Sisesta kasutaja perenimi"
+
+# Muuda väikesteks tähtedeks
+$eesnimi = $eesnimi.ToLower()
+$perenimi = $perenimi.ToLower()
+
+# Loo kasutajanimi
+$kasutajaNimi = "$eesnimi.$perenimi"
+
+Write-Host "Kustutatav kasutaja on $kasutajaNimi"
+
+# Kontroll kas kasutaja eksisteerib
+$kasutaja = Get-LocalUser -Name $kasutajaNimi -ErrorAction SilentlyContinue
+
+if ($kasutaja) {
+
+    # Kustuta kasutaja
+    Remove-LocalUser -Name $kasutajaNimi -ErrorAction SilentlyContinue
+
+    if ($?) {
+        Write-Host "Kasutaja $kasutajaNimi kustutati edukalt." -ForegroundColor Green
+    }
+    else {
+        Write-Host "Kasutaja kustutamine ebaõnnestus." -ForegroundColor Red
+    }
+
+}
+else {
+    Write-Host "Kasutajat $kasutajaNimi ei leitud." -ForegroundColor Yellow
+}
